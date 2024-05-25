55284f0ed83913be42bc6913ee0864703a2048a9
Lionel Clerc <lionel.clerc@protonmail.com>
2024-05-25 13:24:53 +0300

    Add backMeUp.sh script
    makes dry run with --dry flag
    Prompts for username used on the remote host

---
+++ backMeUp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+
+RSYNC_OPTIONS="-chavuP"
+
+if [ "$1" == "--dry" ]; then
+	echo "This is a dry run..."
+	echo ""
+	RSYNC_OPTIONS="${RSYNC_OPTIONS}n"
+else 
+	echo "Synchronization in progress"
+	echo "" 
+fi
+
+
+read -p "Enter the remote username: " REMOTE_USER
+rsync $RSYNC_OPTIONS --delete -e ssh Alpine_Inception:/home/$REMOTE_USER/project .
+ 
